120b76fcdd21b2165840fae28e3a002811aefacb
Craig <indigomx9@gmail.com>
2023-04-06 19:18:36 -0400

    Updated Transform and Main components.

--- src/components/transform/Transform.jsx
+++ src/components/transform/Transform.jsx
@@ -4,7 +4,28 @@ import "./Transform.css";
 export const Transform = () => {
     return (
         <React.Fragment>
-            <h1>Transform</h1>
+            <section className="transform__container">
+                <aside className="transform__text--container">
+                    <div className="transform__text--wrapper">
+                        <div className="transform__header">
+                            Transform your brand
+                        </div>
+                        <div className="transform__body">
+                            We are full-service creative 
+                            agency specializing in helping 
+                            brands grow fast. Engage your 
+                            clients through compelling visuals 
+                            that do most of the marketing for you.
+                        </div>
+                        <a 
+                            href="#" 
+                            className="transform__button"
+                            >LEARN MORE
+                        </a>
+                    </div>
+                </aside>
+                <aside className="transform__image egg"></aside>
+            </section>
         </React.Fragment>
     );
 };
--- src/containers/Main.jsx
+++ src/containers/Main.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Navbar } from "../components/navbar/Navbar";
 import { Hero } from "../components/hero/Hero";
 import { Transform } from "../components/transform/Transform";
 import { StandOut } from "../components/standOut/StandOut";
@@ -8,7 +7,6 @@ export const Main = () => {
     return (
         <React.Fragment>
             <Hero />
-            <Navbar />
             <Transform />
             <StandOut />
         </React.Fragment>
